c7dcd4478dcf376357b7f221b58163571c8c7a41
Frank Gerold <franketsgerold@gmail.com>
2019-10-18 19:12:00 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,78 @@
 def consolidate_cart(cart)
-  # code here
+  list = {}
+  cart.each do
+    |item|
+    item.each do
+      |name, info|
+      if list.key?(name)
+        list[name][:count] += 1
+      else
+        list[name] = info
+        list[name][:count] = 1
+      end
+    end
+  end
+  list
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  coupon_cart = cart.reduce({}) do
+    |memo, (item, details)|
+    memo[item] = details
+    coupons.each do
+      |coupon|
+      if coupon[:item] = memo[item]
+        new_item = item + " W/COUPON"
+        while (memo[item][:count] >= coupon[:num])  do
+          if memo.key?(new_item)
+            memo[new_item][:count] += coupon[:num]
+            memo[item][:count] -= coupon[:num]
+          else
+            memo[new_item] = {}
+            memo[new_item][:count] = coupon[:num]
+            memo[new_item][:clearance] = memo[item][:clearance]
+            memo[new_item][:price] = coupon[:cost]/coupon[:num]
+            memo[item][:count] -= coupon[:num]
+          end
+        end
+      end
+    end
+    memo
+  end
+  coupon_cart
 end
 
 def apply_clearance(cart)
-  # code here
+  clearance_price = 0.80
+  cleared_cart = cart.reduce({})  do
+    |memo, (item, details)|
+    memo[item] = details
+    if details[:clearance]
+      memo[item][:price] *= clearance_price
+    end
+    memo[item][:price].round(2)
+    memo
+  end
+  cleared_cart
 end
 
 def checkout(cart, coupons)
-  # code here
+  discount_minimum = 100.00
+  discount_price = 0.90
+
+  consolidated = consolidate_cart(cart)
+  couponed = apply_coupons(consolidated, coupons)
+  cleared = apply_clearance(couponed)
+
+  cart_total = cleared.reduce(0)  do
+    |memo, (item, details)|
+    memo += (details[:price] * details[:count])
+    memo.round(2)
+    memo
+  end
+  if cart_total > discount_minimum
+    cart_total *= discount_price
+    cart_total.round(2)
+  end
+  cart_total
 end
